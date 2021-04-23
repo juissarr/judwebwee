763f2f8376ccb7bec638d348cbc7dcdcb36236b5
Jude Sarratt <>
2021-04-23 21:22:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,75 @@
+var programCode = function(processingInstance) {
+    with (processingInstance) {
+      size(600, 600);
+      frameRate(60);
+        
+      // Draw Hererererererere:
+      
+
+
+
+<fill>  </fill>
+
+{
+var rdp1 = random(0,600);
+var rdp2 = random(0,600);
+var rdp3 = random(0,600);
+var rdp4 = random(0,600);
+var rdp5 = random(0,600);
+var rdp6 = random(0,600);
+var rdp7 = random(0,600);
+var rdp8 = random(0,600);
+var rdp9 = random(0,600);
+var rdp10 = random(0,600);
+
+var rdp12 = random(0,600);
+var rdp22 = random(0,600);
+var rdp32 = random(0,600);
+var rdp42 = random(0,600);
+var rdp52 = random(0,600);
+var rdp62 = random(0,600);
+var rdp72 = random(0,600);
+var rdp82 = random(0,600);
+var rdp92 = random(0,600);
+var rdp102 = random(0,600);
+}
+
+
+			
+			
+			
+			fill(0,0,255);
+			stroke(255,0,0);
+			
+			strokeWeight(10);
+point(rdp1,rdp12);
+point(rdp2,rdp22);
+point(rdp3,rdp32);
+point(rdp4,rdp42);
+point(rdp5,rdp52);
+point(rdp6,rdp62);
+point(rdp7,rdp72);
+point(rdp8,rdp82);
+point(rdp9,rdp92);
+point(rdp10,rdp102);
+			
+draw = function() {
+	
+
+
+
+
+	
+        
+
+					}
+
+
+
+			//End Herererererererer();
+    }};
+
+  // Get the canvas that ProcessingJS will use
+  var canvas = document.getElementById("mycanvas"); 
+  // Pass the function to ProcessingJS constructor
+  var processingInstance = new Processing(canvas, programCode); 
